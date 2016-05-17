dce9206d5c9c0c80cef6847213a1e4522e147a70
AradoBlitz <schlachtnavaho@gmail.com>
2016-05-17 23:20:17 +0300

    Finished development. No details:-(

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class MergeSort {
@@ -7,15 +8,42 @@ public class MergeSort {
 		return null;
 	}
 
-	public static List<Integer> sort(List<Integer> input, int i, int j) {
+	public static List<Integer> sort(List<Integer> input, int left, int right) {
 		
-		int buff;
-		if(input.get(i).intValue()>input.get(j).intValue()){
-			buff=input.get(i);
-			input.set(i, input.get(j));
-			input.set(j, buff);
-		}
+		if(input.get(left).equals(input.get(right)))
+			return input;
+		
+		int middle = (int)((left+right)*0.5);
+		
+		sort(input,left,middle);
+		sort(input,middle+1,right);
+		merge(input,left,right,middle);
+		
 		return input;
+	}
+
+	public static void merge(List<Integer> input, int left, int right, int middle) {
+		List<Integer> buffer = new ArrayList<>();
+		
+		int i = left;
+		int j = middle + 1;
+		while(i<=middle || j <= right){
+			
+			if(i>middle){
+				buffer.add(input.get(j++));				
+			} else if(j>right){
+				buffer.add(input.get(i++));
+			} else	if(input.get(i).intValue()>input.get(j).intValue()){
+				buffer.add(input.get(j++));
+			} else if(input.get(i).intValue()<input.get(j).intValue()){
+				buffer.add(input.get(i++));					
+			} else {
+				buffer.add(input.get(i++));
+				buffer.add(input.get(j++));
+			}
+		}					
+		for(int k = 0, n = left;k<buffer.size();n++,k++)
+			input.set(n, buffer.get(k));
 	}
 
 }
--- src/TestMergeSort.java
+++ src/TestMergeSort.java
@@ -2,6 +2,7 @@ import static java.util.Arrays.asList;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -13,12 +14,12 @@ public class TestMergeSort {
 
 	List<Integer> input = asList(8,3,2,6,9,7,1,5,4);
 	
-	@Ignore
+	
 	@Test
 	public void sortAll() throws Exception {
 		
 		
-		assertThat(MergeSort.sort(input),equalTo(asList(1,2,3,4,5,6,7,8,9)));
+		assertThat(MergeSort.sort(input,0,8),equalTo(asList(1,2,3,4,5,6,7,8,9)));
 	}
 	
 	@Test
@@ -32,6 +33,32 @@ public class TestMergeSort {
 	public void sortTwoSizeArrays() throws Exception {
 		
 		assertThat(MergeSort.sort(input,0,1),equalTo(asList(3,8,2,6,9,7,1,5,4)));
-		assertThat(MergeSort.sort(input,2,1),equalTo(asList(3,8,2,6,9,7,1,5,4)));
+		assertThat(MergeSort.sort(input,2,3),equalTo(asList(3,8,2,6,9,7,1,5,4)));
+	}
+	
+	@Test
+	public void sortFourElements() throws Exception {
+		input = MergeSort.sort(input,0,1);
+		assertThat(MergeSort.sort(input,0,3),equalTo(asList(2,3,6,8,9,7,1,5,4)));
+	}
+	
+	@Test
+	public void mergeSample() throws Exception {
+		List<Integer> input = Arrays.asList(3, 8, 2, 6, 9, 7, 1, 5, 4);
+		
+		
+		
+		MergeSort.merge(input,0,3,1);			
+		assertThat(input,equalTo(asList(2,3,6,8,9,7,1,5,4)));
+		
+		//Прошел успешно
+		MergeSort.merge(this.input,0,1,0);			
+		assertThat(this.input,equalTo(asList(3,8,2,6,9,7,1,5,4)));
+		
+		//Зациклил программу
+		MergeSort.merge(this.input,2,3,2);			
+		assertThat(this.input,equalTo(asList(3,8,2,6,9,7,1,5,4)));
+		
+		
 	}
 }
